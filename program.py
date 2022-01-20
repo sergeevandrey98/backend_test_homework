296be90d1eb0d2affad58a37e1ef5ee4f64e1d41
Андрей Сергеев <sergeevandrey98@yandex.ru>
2022-01-20 21:15:20 +0300

    upd sprint 2

--- program.py
+++ program.py
@@ -1,1 +1,6 @@
-print('Я домашка'
+from typing import Sequence
+
+def lower_join(seq:Sequence[str]) -> Sequence:
+    """Принимает на вход последовательность и создаёт из неё  
+    строку в нижнем регистре."""
+    return ''.join(seq).lower()
